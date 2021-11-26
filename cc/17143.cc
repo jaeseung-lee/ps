dd11c126558f3f5ffc6f539bd35fd25c48fcca3b
jaeseung-lee <kevin0969@naver.com>
2021-11-26 14:23:50 +0900

    17143(unsolved)

--- cc/17143.cc
+++ cc/17143.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +10,8 @@ enum direction { UP = 1, DOWN = 2, RIGHT = 3, LEFT = 4 };
 int R, C, M;
 
 struct status {
-  bool isShark;
-  int velocity;
-  int dir;
-  int size;
+  bool is_shark;
+  int shark_index;
 };
 
 int ans = 0;
@@ -21,23 +20,47 @@ void input();
 void solve();
 
 status board[101][101];
-status make_status(int velocity, int dir, int size) {
+status makeStatus(int shark_index) {
   status result;
-  result.velocity = velocity;
-  result.dir = dir;
-  result.size = size;
-  result.isShark = true;
+  result.is_shark = true;
+  result.shark_index = shark_index;
   return result;
 }
 
-status make_status() {
+status makeStatus() {
   status result;
-  result.velocity = 0;
-  result.isShark = false;
-  result.dir = -1;
-  result.size = 0;
+  result.is_shark = false;
+  result.shark_index = -1;
   return result;
 }
+
+struct sharkStatus {
+  bool is_alive;
+  int velocity;
+  int dir;
+  int size;
+  int r;
+  int c;
+};
+
+sharkStatus makeSharkStatus(int velocity, int dir, int size, int r, int c) {
+  sharkStatus result;
+  result.velocity = velocity;
+  result.dir = dir;
+  result.size = size;
+  result.r = r;
+  result.c = c;
+  return result;
+}
+
+void makeSharkDead(sharkStatus& target) { target.is_alive = false; }
+void moveShark(sharkStatus& target, int new_dir, int new_r, int new_c) {
+  target.dir = new_dir;
+  target.r = new_r;
+  target.c = new_c;
+}
+
+vector<sharkStatus> sharkSpot;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -55,12 +78,13 @@ void input() {
   int r, c, s, d, z;
   for (int i = 1; i <= R; i++) {
     for (int j = 1; j <= C; j++) {
-      board[i][j] = make_status();
+      board[i][j] = makeStatus();
     }
   }
   for (int i = 0; i < M; i++) {
     cin >> r >> c >> s >> d >> z;
-    board[r][c] = make_status(s, d, z);
+    board[r][c] = makeStatus(sharkSpot.size());
+    sharkSpot.push_back(makeSharkStatus(s, d, z, r, c));
   }
 }
 
@@ -68,14 +92,71 @@ void solve() {
   for (int i = 1; i <= C; i++) {
     // 땅과 가장 가까운 상어 잡어먹기
     for (int j = 1; j <= R; j++) {
-      if (board[j][i].isShark) {
-        ans += board[j][i].size;
-        board[j][i] = make_status();
+      if (board[j][i].is_shark) {
+        ans += sharkSpot[board[i][j].shark_index].size;
+        board[j][i] = makeStatus();
+        sharkSpot[board[j][i].shark_index].is_alive = false;
         break;
       }
     }
 
     // 상어들이 움직임
-    // 상어 id도 주자.
+    // 일단 상어들이 움직인 위치를 sharkSpot에 저장시키고
+    // board는 비워둠
+    for (auto shark : sharkSpot) {
+      if (shark.is_alive) {
+        switch (shark.dir) {
+          case UP:
+            if ((C - shark.c - 1 - shark.velocity) % C) break;
+
+          case DOWN:
+
+            break;
+
+          case LEFT:
+
+            break;
+
+          case RIGHT:
+
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+
+    // 상어들을 sharkSpot을 통하여 board에 채워넣는데, 이미 채워져있으면 더 큰
+    // 거로 채워넣는다.
+    for (int shark_index = 0; shark_index < sharkSpot.size(); shark_index++) {
+      if (sharkSpot[shark_index].is_alive) {
+        // 비어있는 칸이면 그냥 집어넣는다.
+        if (!board[sharkSpot[shark_index].r][sharkSpot[shark_index].c]
+                 .is_shark) {
+          board[sharkSpot[shark_index].r][sharkSpot[shark_index].c].is_shark =
+              true;
+          board[sharkSpot[shark_index].r][sharkSpot[shark_index].c]
+              .shark_index = shark_index;
+        } else {
+          // 비어있지 않으면 더 큰 걸 집어넣고, 작은 건 dead로 바꾼다.
+          // board에 있는 상어가 더 크다면, sharkSpot 상어를 죽임
+          if (sharkSpot[board[sharkSpot[shark_index].r]
+                             [sharkSpot[shark_index].c]
+                                 .shark_index]
+                  .size > sharkSpot[shark_index].size) {
+            sharkSpot[shark_index].is_alive = false;
+          } else {
+            // 들어올 상어가 더 크다면 기존의 상어를 죽이고, 그 board의 index
+            // 정보를 갱신
+            sharkSpot[board[sharkSpot[shark_index].r][sharkSpot[shark_index].c]
+                          .shark_index]
+                .is_alive = false;
+            board[sharkSpot[shark_index].r][sharkSpot[shark_index].c]
+                .shark_index = shark_index;
+          }
+        }
+      }
+    }
   }
 }
